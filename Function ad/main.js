d42b72d2b98597ea4850f0959592370ea3a1abcf
Henrique Anjos <59669147+Licaesar@users.noreply.github.com>
2022-01-11 11:47:18 -0400

    add function

---
+++ Function ad/main.js
@@ -0,0 +1,34 @@
+// Declaração de função (Function hoisting) o motor do javascrip sempre coloca as funções no top da execulção
+function falaOi() {
+    console.log('Oie')
+}
+falaOi();
+
+// first-class objects (Objetos de primeira classe)
+// Function expression, um variavel receber um função
+const souUmDado = function() {
+    console.log('Sou um dado')
+}
+
+function execultaFucao(funcao) {
+    funcao();
+}
+execultaFucao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma Arrow function');
+};
+funcaoArrow();
+
+// Todas essas funções são de primeira classe, tranto ela como dado
+
+
+// Função em Objeto
+const obj = {
+    falar() {
+        console.log('Estou falando...');
+    }
+};
+
+obj.falar();
--- Switch-Case/main.js
+++ Switch-Case/main.js
@@ -1,6 +1,39 @@
-const data = new Date();
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+    }
+}
+
+const data = new Date('2000-01-05 00:00:00');
 const diaSemana = data.getDay();
-let diaSemanaTexto;
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
 
 /* if (diaSemana === 0) {
     diaSemanaTexto = 'Domingo';
@@ -17,33 +50,3 @@ let diaSemanaTexto;
 } else if (diaSemana === 6) {
     diaSemanaTexto = 'Sabado';
 } */
-
-switch (diaSemana) {
-    case 0:
-        diaSemanaTexto = 'Domingo';
-        break;
-    case 1:
-        diaSemanaTexto = 'Segunda';
-        break;
-    case 2:
-        diaSemanaTexto = 'Terça';
-        break;
-
-    case 3:
-        diaSemanaTexto = 'Quarta';
-        break;
-    case 4:
-        diaSemanaTexto = 'Quinta';
-        break;
-    case 5:
-        diaSemanaTexto = 'Sexta';
-        break;
-    case 6:
-        diaSemanaTexto = 'Sabado';
-        break;
-    default:
-        diaSemanaTexto = '';
-}
-
-
-console.log(diaSemanaTexto);
